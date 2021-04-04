e52586ff909f6a2c021370d2ce8a18041e07b44a
Brendan Manning <>
2021-04-04 11:37:41 -0400

    Some other changes

--- MapReduce/src/com/brendanmanning/HW3/WordCountMapper.java
+++ MapReduce/src/com/brendanmanning/HW3/WordCountMapper.java
@@ -9,14 +9,23 @@ import org.apache.hadoop.io.LongWritable;
 
 public class WordCountMapper extends Mapper <LongWritable, Text, Text, IntWritable>
 {
-    private Text wordToken = new Text();
+    private final static Text word = new Text();
+    private final static IntWritable count = new IntWritable(1);
+
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
     {
-        StringTokenizer tokens = new StringTokenizer(value.toString()); //Dividing String into tokens
-        while (tokens.hasMoreTokens())
-        {
-            wordToken.set(tokens.nextToken());
-            context.write(wordToken, new IntWritable(1));
+        // Create a string tokenizer on the lowercase content using only the space
+        StringTokenizer itr = new StringTokenizer(sanitize(value.toString()), " ");
+
+        // Loop through all the available tokens
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, count);
         }
     }
+
+    private String sanitize(String str) {
+        return str.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+    }
+
 }
--- MapReduce/src/com/brendanmanning/HW3/WordCountReducer.java
+++ MapReduce/src/com/brendanmanning/HW3/WordCountReducer.java
@@ -7,15 +7,20 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class WordCountReducer extends Reducer <Text, IntWritable, Text, IntWritable>
 {
     private IntWritable count = new IntWritable();
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
-    {
-// gurukul [1 1 1 1 1 1….]
-        int valueSum = 0;
-        for (IntWritable val : values)
-        {
-            valueSum += val.get();
+
+    public void reduce(
+        Text key,
+        Iterable<IntWritable> values,
+        Context context
+    ) throws IOException, InterruptedException {
+
+        int cnt = 0;
+        for (IntWritable val : values) {
+            cnt += val.get();
         }
-        count.set(valueSum);
+
+        count.set(cnt);
         context.write(key, count);
+
     }
 }
